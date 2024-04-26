a309e4174ce1fec637b1a65f56ffd1bf6ce96b27
ezequielsacchi <ezesacchi460@gmail.com>
2024-04-26 20:30:37 -0300

    terminando chat bidireccional cli/serv sin threading

--- pruebasCliServ/server.py
+++ pruebasCliServ/server.py
@@ -1,6 +1,53 @@
 import socket
-import threading
 
+# Configurar el servidor
+host = "127.0.0.1"
+port = 5555
+
+# Crear un socket TCP/IP
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        
+    # Enlazar el socket al host y al puerto
+    server_socket.bind((host, port))
+        
+    print(f"Escuchando en el puerto {port} si")
+    # Escuchar conexiones entrantes
+    server_socket.listen()
+    print("Servidor escuchando en {}:{}...".format(host, port))
+
+    # Aceptar la conexión entrante
+    client_socket, client_address = server_socket.accept()
+    #print("Cliente {}:{} conectado.".format(client_address[0], client_address[1]))
+
+        # Iniciar el chat
+    while True:
+            # Recibir mensaje del cliente
+        client_message = client_socket.recv(1024).decode("utf-8")
+        if not client_message:
+            break
+            #print(f"{client_address[0]} : {data.decode('utf-8')}")
+        print("Cliente: {}".format(client_message))
+
+            # Enviar mensaje al cliente
+        server_message = input("Servidor: ")
+        client_socket.sendall(server_message.encode("utf-8"))
+
+    # Cerrar la conexión
+client_socket.close()
+server_socket.close()
+
+
+"""
+
+socket.AF_INET: Este argumento especifica que el socket será de tipo AF_INET, que es la familia de direcciones para IPv4. 
+Indica que el socket utilizará direcciones IPv4 para la comunicación en red.
+
+socket.SOCK_STREAM: Este argumento especifica el tipo de socket, en este caso SOCK_STREAM, que indica que el socket será de tipo flujo. 
+Los sockets de flujo proporcionan una conexión bidireccional 
+
+"""
+
+"""
 HOST = "127.0.0.1"
 PORT = 38616
 
@@ -18,6 +65,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 break
             #s.send(data)
             print(f"{addr[0]} : {data.decode('utf-8')}")
+"""
 
 
 
