22001c630faeeac616800505287077a8ad6ffea7
Tsifok <hermanmatias03@gmail.com>
2024-05-13 15:52:43 -0300

    codigo de broadcasting + threading

--- Cli-Serv-threading/client.py
+++ Cli-Serv-threading/client.py
@@ -1,54 +1,31 @@
 import socket
-
-host = "127.0.0.1"
-port = 5555
-
-# Crear un socket TCP/IP
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-
-    # Conectar el socket al servidor
-    client_socket.connect((host, port))
-    print("Conectado al servidor.")
-
-    # Iniciar el chat
-    while True:
-        # Enviar mensaje al servidor
-        client_message = input("Cliente: ")
-        client_socket.sendall(client_message.encode("utf-8"))
-
-        # Salir si el mensaje es "adios"
-        if client_message.lower() == "adios":
-            break
-
-        # Recibir mensaje del servidor
-        server_message = client_socket.recv(1024).decode("utf-8")
-        print("Servidor:", server_message)
-
-    # Cerrar la conexión
-    client_socket.close()
-
-
-"""
-
-import socket
 import threading
 
-HOST = "127.0.0.1" 
-PORT = 38616
+nickname = input("Input your nickname: ")
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("127.0.0.1", 55555))
+
+def receive():
     while True:
-        msj = input("msj: ")
-        s.send(bytes(msj, "utf-8"))
-        data = s.recv(1024)
-   
-print(f"Recibido {data!r}")    
+        try:
+            mssg = client.recv(1024).decode('ascii')
+            if mssg == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(mssg)
+        except: 
+            print("An error ocurred!")
+            client.close()
+            break
 
-1 Cliente(s) servidor threading, el cliente se debe autentificar (lo primero que aparece usuario contraseña) si no esta autentificado cierra conexion
-2 El cliente a travez de un menu (opc 1 2 3 4 ) 
-    1 chat simple cliente servidor ()
-    2 Enviar un mensaje broker(a todos los clientes conectados)
+def write():
+    while True:
+        mssg = f"{nickname} : {input("")}"
+        client.send(mssg.encode('ascii'))
 
+receive_thread = threading.Thread(target="receive")
+receive_thread.start()
 
-"""
+write_thread = threading.Thread(target="write")
+write_thread.start()
--- Cli-Serv-threading/server.py
+++ Cli-Serv-threading/server.py
@@ -1,68 +1,49 @@
-import socket
+import threading
+import socket 
 
-# Configurar el servidor
-host = "127.0.0.1"
-port = 5555
+host = '127.0.0.1'
+port =  55555
 
-# Crear un socket TCP/IP
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        
-    # Enlazar el socket al host y al puerto
-    server_socket.bind((host, port))
-        
-    print(f"Escuchando en el puerto {port} si")
-    # Escuchar conexiones entrantes
-    server_socket.listen()
-    print("Servidor escuchando en {}:{}...".format(host, port))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host,port))
+server.listen()
 
-    # Aceptar la conexión entrante
-    client_socket, client_address = server_socket.accept()
-    #print("Cliente {}:{} conectado.".format(client_address[0], client_address[1]))
+clients = []
+nicknames = []
 
-        # Iniciar el chat
-    while True:
-            # Recibir mensaje del cliente
-        client_message = client_socket.recv(1024).decode("utf-8")
-        if not client_message:
+def broadcast(mssg):
+    for client in clients:
+        client.send(mssg)
+
+def handle(client): 
+    while True: 
+        try: 
+            mssg = client.recv(1024)
+            broadcast(mssg)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} left the chat!'. encode('ascii'))
+            nicknames.remove(nickname)
             break
-        #print(f"{client_address[0]} : {data.decode('utf-8')}")
-        print("Cliente: {}".format(client_message))
 
-            # Enviar mensaje al cliente
-        server_message = input("Servidor: ")
-        client_socket.sendall(server_message.encode("utf-8"))
+def recive(): 
+    while True:
+        client, addres = server.accept()
+        print(f"Connected with {str(addres)}")
 
-    # Cerrar la conexión
-client_socket.close()
-server_socket.close()
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recive(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+        print(f'Nickname of the client is {nickname}')
+        broadcast(f'{nickname} joinded the chat!'.encode('ascii'))
+        client.send("Connected to the server")
 
+        thread = threading.Thread(target="handle", args=(client,))
+        thread.start()
 
-"""
-
-socket.AF_INET: Este argumento especifica que el socket será de tipo AF_INET, que es la familia de direcciones para IPv4. 
-Indica que el socket utilizará direcciones IPv4 para la comunicación en red.
-
-socket.SOCK_STREAM: Este argumento especifica el tipo de socket, en este caso SOCK_STREAM, que indica que el socket será de tipo flujo. 
-Los sockets de flujo proporcionan una conexión bidireccional 
-
-"""
-
-"""
-HOST = "127.0.0.1"
-PORT = 38616
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    print(f"Escuchando en el puerto {PORT} si")
-    s.listen()
-    conn, addr  = s.accept()
-    with conn:
-        print(f"Conectado a {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                print("cloasing conn")
-                break
-            #s.send(data)
-            print(f"{addr[0]} : {data.decode('utf-8')}")
-"""
+print("Server is listening .....")
+recive()
